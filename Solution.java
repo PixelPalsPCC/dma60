2f3d6e013b228980ce6f3032fafd9904541a2eb7
George McKinney <george@hookstream.com>
2026-03-16 01:05:28 -0700

    fix to run on non-Windows computers

--- Solution.java
+++ Solution.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in).useDelimiter("\r\n");
+        Scanner scan = new Scanner(System.in).useDelimiter("\r?\n");
         int i = scan.nextInt();
+        System.out.println("Int: " + i);
 
         // Write your code here.
         double d = scan.nextDouble();
-        String s = scan.next();
-        scan.close();
-        System.out.println("String: " + s);
         System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+        String s = scan.next();
+        System.out.println("String: " + s);
+        scan.close();
     }
 }
 // https://www.hackerrank.com/challenges/java-stdin-stdout/problem?isFullScreen=true  
